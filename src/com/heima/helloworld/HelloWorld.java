bedad1faa85a717df5ac6d504bf36bd57ef3fbc0
lanjainhua <1135790798@qq.com>
2019-06-30 18:45:32 +0800

    第一次

---
+++ src/com/heima/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.heima.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("楼高终究误少年，自由早晚乱余生");
+		
+	}
+
+}
